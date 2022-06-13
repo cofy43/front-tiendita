60d980ddb9d1c2c1895955cc0efa4687fac92107
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-06-13 18:24:30 -0500

    barra superior completa

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Sidebar from "../components/sidebar/sidebar";
@@ -5,21 +6,59 @@ import Home from "../components/home/Home";
 import Contact from "../components/contact/Contact";
 import About from "../components/about/About";
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import Grid from '@mui/material/Grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
+
 import "./main.css";
 
 function Main() {
+  const theme = useTheme();
+  const breakpoint = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <div className="app">
       <HashRouter>
         <div className="sidebar">
           <Sidebar />
-        </div>        
+        </div>
         <div className="content">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
+          <nav>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid
+                container
+                spacing={2}
+                direction={breakpoint ? "column" : "row"}
+                justify="center"
+              >
+                <Grid item xs={6} md={9}>
+                  <span style={breakpoint ? {paddingLeft: 'calc(10%)'} : {paddingLeft: 'calc(50%)'}} className="title">Tiendita</span>
+                </Grid>
+                <Grid item xs={6} md={3}>
+                  <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                    <SearchIcon
+                      sx={{ color: "action.active", mr: 1, my: 0.5 }}
+                    />
+                    <TextField
+                      id="input-with-sx"
+                      label="Búsqueda rápida"
+                      variant="standard"
+                    />
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          </nav>
+          <div style={breakpoint ? {marginTop: '5rem'}: {marginTop: '1rem'}}>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
         </div>
       </HashRouter>
     </div>
