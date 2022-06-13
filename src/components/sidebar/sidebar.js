e8fc6f630a65befc62f99a794050e45736a5072c
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-06-13 16:40:42 -0500

    reparacion de pantalla completa

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,31 +1,43 @@
+import React from "react";
+
 import  { NavLink } from "react-router-dom";
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import MenuIcon from '@mui/icons-material/Menu';
 import 'react-pro-sidebar/dist/css/styles.css';
 
 function Sidebar() {
+    const [menuVisible, setMenuVisible] = React.useState(true);
+
     return (
-        // <div className="navbar">
-        //     <h1>Navbar</h1>
-        //     <ul>
-        //         <li><NavLink to="/">Home</NavLink></li>
-        //         <li><NavLink to="/contact">Contact</NavLink></li>
-        //         <li><NavLink to="/about">About</NavLink></li>
-        //     </ul>
-        // </div>
-        <ProSidebar>
-            <Menu iconShape="square">
-                <MenuItem >
-                    <NavLink to="/">Productos</NavLink>
-                </MenuItem>
-                <MenuItem >
-                    <NavLink to="/contact">Ventas</NavLink>
-                </MenuItem>
-                <MenuItem >
-                    <NavLink to="/about">Proveedores</NavLink>
-                </MenuItem>                
-            </Menu>
-        </ProSidebar>
-    )
+      // <div className="navbar">
+      //     <h1>Navbar</h1>
+      //     <ul>
+      //         <li><NavLink to="/">Home</NavLink></li>
+      //         <li><NavLink to="/contact">Contact</NavLink></li>
+      //         <li><NavLink to="/about">About</NavLink></li>
+      //     </ul>
+      // </div>
+      <ProSidebar breakPoint="md" collapsed={menuVisible}>
+        <Menu iconShape="square">
+          <MenuItem
+            icon={<MenuIcon style={{ height: "35px", width: "35px" }} />}            
+            onClick={() => {
+              setMenuVisible(!menuVisible);
+            }}
+          >
+          </MenuItem>
+          <MenuItem>
+            <NavLink to="/">Productos</NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink to="/contact">Ventas</NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink to="/about">Proveedores</NavLink>
+          </MenuItem>
+        </Menu>
+      </ProSidebar>
+    );
 }
 
 export default Sidebar;
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,31 +1,29 @@
 import  { HashRouter, Routes, Route } from "react-router-dom";
 
 import Sidebar from '../components/sidebar/sidebar'
-
 import Home from "../components/home/Home";
 import Contact from "../components/contact/Contact";
 import About from "../components/about/About";
 
-import './main.css';
+import "./main.css";
 
 function Main() {
-    return (
-        <div className="app">
-            <HashRouter>
-                <div className="sidebar">
-                    <Sidebar/>
-                </div>                
-                <div className="content">
-                
-                    <Routes>
-                        <Route exact path="/" element={<Home/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                        <Route path="/about" element={<About/>}/>
-                    </Routes>
-                </div>
-            </HashRouter>
+  return (
+    <div className="app">
+      <HashRouter>
+        <div className="sidebar">
+          <Sidebar />
         </div>
-    )
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default Main;
